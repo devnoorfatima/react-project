5fc951afaba8615a28d91579d12265226bcfd4a8
Noor Fatima <devnoorfatima@gmail.com>
2019-08-30 15:44:41 +0500

    Updates

---
+++ precache-manifest.40a55c9a308c4baf004cb5ece0a5bc5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a54ebb76cbb06400fd1",
+    "url": "/react-project/static/js/runtime~main.0a54ebb7.js"
+  },
+  {
+    "revision": "5f37a415dd9a1b7141d0",
+    "url": "/react-project/static/js/main.5f37a415.chunk.js"
+  },
+  {
+    "revision": "2605c80301257bfc8347",
+    "url": "/react-project/static/js/1.2605c803.chunk.js"
+  },
+  {
+    "revision": "5f37a415dd9a1b7141d0",
+    "url": "/react-project/static/css/main.b64b73b5.chunk.css"
+  },
+  {
+    "revision": "2605c80301257bfc8347",
+    "url": "/react-project/static/css/1.b29565a1.chunk.css"
+  },
+  {
+    "revision": "07d31f2e24936f8d7ff83c36ab4d2968",
+    "url": "/react-project/index.html"
+  }
+];
